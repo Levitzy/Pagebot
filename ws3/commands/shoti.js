4c24720c7c60dd5f75c3884ea8f52f377aaae4c6
Levitzy <levitzy123@gmail.com>
2024-10-25 04:21:42 +0800

    fix

--- ws3/commands/shoti.js
+++ ws3/commands/shoti.js
@@ -10,12 +10,13 @@ module.exports = {
   description: "Send a random Shoti video",
   async run({ api, event, send }) {
     try {
-      // Notify user that the video is being downloaded
+      // Notify the user that the video is being downloaded
       await send("Downloading random Shoti video. Please wait...");
 
       // Fetch random Shoti video data
       const response = await axios.get('https://shoti.kenliejugarap.com/getvideo.php?apikey=shoti-0763839a3b9de35ae3da73816d087d57d1bbae9f8997d9ebd0da823850fb80917e69d239a7f7db34b4d139a5e3b02658ed26f49928e5ab40f57c9798c9ae7290c536d8378ea8b01399723aaf35f62fae7c58d08f04');
 
+      // Check if the response status is valid and video data is available
       if (response.data.status) {
         const videoUrl = response.data.videoDownloadLink;
         const videoTitle = response.data.title;
@@ -30,26 +31,26 @@ module.exports = {
           responseType: 'stream'
         });
 
-        // Save the video to the temporary file
+        // Pipe the video data into a temporary file
         const writer = fs.createWriteStream(tempFilePath);
         videoStream.data.pipe(writer);
 
-        // Return a promise that resolves when the file is finished writing
+        // Wait for the file writing to finish
         await new Promise((resolve, reject) => {
           writer.on('finish', resolve);
           writer.on('error', reject);
         });
 
         // Send the video as an attachment
-        const videoMessage = {
+        const message = {
+          body: `Here is your random Shoti video: ${videoTitle}`,
           attachment: fs.createReadStream(tempFilePath),
-          type: 'video',
-          body: `Here is your random Shoti video: ${videoTitle}`
+          type: 'video'
         };
 
-        await send(videoMessage);
+        await api.sendMessage(message, event.threadID);
 
-        // Clean up the temporary file
+        // Clean up the temporary file after sending the video
         fs.unlinkSync(tempFilePath);
       } else {
         send("Failed to fetch a Shoti video. Please try again later.");
